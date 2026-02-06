2540926bce415ae67433f1571536c7e9489c9426
IMTIAZ ADAR <imtiazadar2000@gmail.com>
2026-02-07 00:31:59 +0600

    Update config.py

--- app/config/config.py
+++ app/config/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 from pydantic import Field
 class Settings(BaseSettings):
     database_url: str = Field(..., alias="DATABASE_URL")
-    echo: bool = Field(default=False, alias="ECHO")
+    echo: bool = Field(..., alias="ECHO")
     secret_key: str = Field(..., alias="SECRET_KEY")
     algorithm: str = Field(default="HS256", alias="ALGORITHM")
     access_token_expire_minutes: int = Field(..., alias="ACCESS_TOKEN_EXPIRE_MINUTES")
